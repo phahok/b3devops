e103fbee28e3e761cbbea3a949576107c91ff5b6
Jose Renato Ferreira <joserenatosp@outlook.com>
2020-05-31 00:35:38 -0300

    update hook
    teste connection build triggers

--- testehook.sh
+++ testehook.sh
@@ -1,5 +1,6 @@
 Test 01 webhook - SAS VIYA PIPELINE 30.05.20 22:20h - Error
 Test 02 webhook - SAS VIYA PIPELINE 30.05.20 22:29h - Error
-Test 03 webhook - SAS VIYA PIPELINE 30.05.20 22:32h
+Test 03 webhook - SAS VIYA PIPELINE 30.05.20 22:32h - Error
+Test 03 webhook - SAS VIYA PIPELINE 31.05.20 00:32h
 
 
