46c0960a2a3b97b6c90eaca62305844ab804d3c8
U-CARYNT\joyfer <joyfer@brzsp767.na.SAS.com>
2020-11-03 11:55:33 -0300

    teste vale

--- Test.sas
+++
@@ -1,7 +0,0 @@
-cas auto authinfo="/home/sas/.authinfo" host='sasviyahost.ec2.internal' port=5570 sessopts=(caslib=casuser timeout=1800 locale="en_US");
-caslib _ALL_ assign;
-
-
-proc casutil;
-	LIST files incaslib="PUBLIC";
-run;
--- testehook.sh
+++
@@ -1,6 +0,0 @@
-Test 01 webhook - SAS VIYA PIPELINE 30.05.20 22:20h - Error
-Test 02 webhook - SAS VIYA PIPELINE 30.05.20 22:29h - Error
-Test 03 webhook - SAS VIYA PIPELINE 30.05.20 22:32h - Error
-Test 03 webhook - SAS VIYA PIPELINE 31.05.20 00:32h
-
-
